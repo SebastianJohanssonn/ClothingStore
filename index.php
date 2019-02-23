f57f5ba0556a8665c4e9547d0e88a2d84b0df2b1
Ali Ezadkhaha <43235023+hodhod212@users.noreply.github.com>
2019-02-23 03:04:23 +0100

    style på footer.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+    <script src="script.js"></script>
+    </head>
+    <body>
+        <div class="footer">
+        <?php include './includes/footer.php'; ?>
+        </div>
+    </body>
+</html>
