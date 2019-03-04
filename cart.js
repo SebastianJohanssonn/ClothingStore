aef7573652b568b8e80c3e1bdda0bd020e8f6708
vogelsara <sari.vogel.94@gmail.com>
2019-03-04 21:30:09 +0100

    Add deleteFromShoppingcart() function to cart.js

--- cart.js
+++ cart.js
@@ -14,3 +14,19 @@ function addToShoppingcart(addProductButton) {
         console.log(json);           
     });
 }
+
+function deleteFromShoppingcart(deleteProductButton) {
+
+    var productId = deleteProductButton.id;
+    var queryString = "productId="+productId;
+
+    fetch("cartBackend.php", {
+        method: 'DELETE',
+        credentials: 'include',
+        body: queryString
+    })
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json);           
+    });
+}
--- cart.php
+++ cart.php
@@ -1,5 +1,6 @@
 <?php include ('includes/header.php') ?>
 <div class="mainBody">
     <h2>Shopping Cart</h2>
+    <button id="1" onclick="deleteFromShoppingcart(this)">Delete</button>
 </div>
 <?php include ('includes/footer.php') ?>
