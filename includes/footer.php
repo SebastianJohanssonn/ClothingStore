395be3f3018e83a2660fc0c655104eae552ae6c2
Anel Poturovic <you@example.com>
2019-03-04 13:46:47 +0100

    added alive year

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,25 @@
+<?php
+function ClothingStore($startYear) {
+    $currentYear = date('Y');
+    if ($startYear < $currentYear) {
+        $currentYear = date('y');
+        return "&copy; $startYear&ndash;$currentYear";
+    } else {
+        return "&copy; $startYear";
+    }
+}
+echo ClothingStore(2018);
+
+?>
+
 <footer>
     <ul>
         <li>info@cs.com</li>
-        <li id=footerYear>2019</li>
+        <li id=footerYear> <?php echo ClothingStore(2018); ?></li>
         <li>01 23 456 789</li>
     </ul>
 </footer>
 </body>
 
 </html>
+
