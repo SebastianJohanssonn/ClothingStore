8d4a6aff2656b7ce0466ace8aa0e6d74915a7ea5
vogelsara <sari.vogel.94@gmail.com>
2019-03-04 20:29:13 +0100

    Delete the header picture from cart.php

--- cart.php
+++ cart.php
@@ -1,6 +1,5 @@
 <?php include ('includes/header.php') ?>
 <div class="mainBody">
-    <img src="clothingStoreImg.jpg" alt="imageHomePage">
     <button onclick="addToShoppingcart(this)" id="1">Add</button>
     <h2>Shopping Cart</h2>
 </div>
