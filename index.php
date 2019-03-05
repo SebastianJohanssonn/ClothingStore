8eb2c34bb9e1ac27fd99a455eb2b7abbe267de1f
vogelsara <sari.vogel.94@gmail.com>
2019-03-05 21:52:39 +0100

    Delete body tags from header.php and footer.php and add body tags to index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php include ('includes/header.php') ?>
+<body>
 <div class="mainBody">
     <img src="clothingStoreImg.jpg" alt="imageHomePage">
     <h2>Our Values</h2>
     <p id= "valuesText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium provident labore, repellendus voluptate alias quod, obcaecati vitae nihil distinctio ipsum accusamus! Nisi harum rem reprehenderit ipsam numquam voluptatum nostrum dicta?</p>
 </div>
+</body>
 <?php include ('includes/footer.php') ?>
 
