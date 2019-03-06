df852c1bfce0f9fed5ecbe38169a21397b5730db
vogelsara <sari.vogel.94@gmail.com>
2019-03-06 22:15:24 +0100

    Display shoppingCart and fix delete from shopping cart

--- cart.js
+++ cart.js
@@ -39,17 +39,43 @@ function displayShoppingcart() {
     .then((shoppingCart) => {
         console.log(shoppingCart);
         for (var productId in shoppingCart) {
+            if (shoppingCart[productId] != null) {
+                fetch("api/get.php?productId="+productId, {
+                    method: 'GET',
+                    credentials: 'include'
+                })
+                .then((response) => response.json())
+                .then((productInfo) => {
+                    console.log(productInfo);
+                    var chosenProduct = document.createElement("div");
+                    chosenProduct.classList.add("chosenProduct");
+                    
+                    var chosenProductImage = document.createElement("img");
+                    chosenProductImage.src = "data:image/jpeg;base64," + productInfo["image"];
+                    chosenProduct.appendChild(chosenProductImage);
 
-            fetch("api/get.php?productId="+productId, {
-                method: 'GET',
-                credentials: 'include'
-            })
-            .then((response) => response.json())
-            .then((productInfo) => {
-                console.log(productInfo);
-                var productContainer = document.getElementById("divOfChosenProducts");           
-            });
+                    var nameOfChosenProduct = document.createElement("h3");
+                    nameOfChosenProduct.innerText = productInfo["name"];
+                    chosenProduct.appendChild(nameOfChosenProduct);
 
+                    var priceOfChosenProduct = document.createElement("p");
+                    priceOfChosenProduct.innerText = productInfo["price"] + " kr";
+                    chosenProduct.appendChild(priceOfChosenProduct);
+
+                    var deleteButton = document.createElement("button");
+                    deleteButton.classList.add("deleteButton");
+                    deleteButton.id = productInfo["productId"];
+                    deleteButton.innerText = "Delete";
+                    deleteButton.setAttribute("onclick", "deleteFromShoppingcart(this)");
+                    chosenProduct.appendChild(deleteButton);
+
+                    var productContainer = document.getElementById("divOfChosenProducts");
+
+                    productContainer.appendChild(chosenProduct);
+
+
+                });
+            } 
         }   
     });
 
--- cart.php
+++ cart.php
@@ -2,14 +2,8 @@
 <body onload="displayShoppingcart()">
 <div class="mainBody">
     <h2>Shopping Cart</h2>
-    <button id="1" onclick="deleteFromShoppingcart(this)">Delete</button>
     <div id="divOfChosenProducts">
-        <div class="chosenProduct">
-            <img src="clothingStoreImg.jpg">
-            <h3></h3>
-            <p></p>
-            <button>Delete</button>
-        </div>
+        
     </div>
 </div>
 </body>
