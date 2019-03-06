46139a777f769c389c0908fe3dd0df2d62b62793
Henric <hencr33k@gmail.com>
2019-03-06 19:51:35 +0100

    optimized homepage for Desktop

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <link rel="stylesheet" type="text/css" href="homePage.css">
+    <link rel="stylesheet" type="text/css" href="homePageDesktop.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script>
@@ -20,17 +21,23 @@
             $(".mainBody, h1, footer").click(function(){
                 $("#dropdown-content1").hide();
             })
+            $("#dropbtnDesktop").hover(function(){
+                $(".dropdownDesktop").show();
+            });
+            $(".mainBody, h1, footer").hover(function(){
+                $(".dropdownDesktop").hide();
+            })
+            
         });
     </script>
 </head>
 <body>
 <header>
-    <h1 id="headerh1">Clothing Store</h1>
     <a id="cartLink "href="#"><i id="cart" class="fas fa-shopping-cart"></i></a>
-    <nav>
+    <h1 id="headerh1">Clothing Store</h1>
+    <nav class="mobileNav">
         <div class="dropdown">
             <i id="dropbtn1" class="dropbtn fas fa-bars"></i>
-
         </div>
         <div id="dropdown-content1" class="dropdown-content">
             <a href="clothes.php">Clothes</a>
@@ -39,5 +46,16 @@
             <a href="login.php">Log in</a>
             <a href="register.php">Register</a>
         </div>
+    </nav>
+    <nav class="desktopNav">
+        <a href="index.php">Home</a>
+            <span id="dropbtnDesktop">Products</span>
+        <div class="dropdown-content dropdownDesktop">
+            <a href="clothes.php">Clothes</a>
+            <a href="accesories.php">Accesories</a>
+            <a href="shoes.php">Shoes</a>
+        </div>
+        <a href="register.php">Register</a>
+
     </nav>
 </header>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include ('includes/header.php') ?>
 <div class="mainBody">
-    <img src="clothingStoreImg.jpg" alt="imageHomePage">
+        <img id="fullSizeImg" src="clothingStoreImg.jpg" alt="imageHomePage">
+        <img id="croppedImg" src="croppedImg.png" alt="imageHomePage">
     <h2>Our Values</h2>
     <p id= "valuesText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium provident labore, repellendus voluptate alias quod, obcaecati vitae nihil distinctio ipsum accusamus! Nisi harum rem reprehenderit ipsam numquam voluptatum nostrum dicta?</p>
 </div>
