30976743ed0af09fb2ee570ab9e5753e4ceb48a1
Anel Poturovic <you@example.com>
2019-03-06 17:24:14 +0100

    removed extra head

--- admin.php
+++ admin.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/adminStyle.css">
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="crossorigin="anonymous"></script>
-    <script src="./script/adminScript.js"></script>
-    <title>Admin Page</title>
-</head>
-<body>
-<?php
-    include "includes/header.php";
-    ?>
-    <body>
-<?php 
-	include('functions.php');
-
-	if (!isAdmin()) {
-		$_SESSION['msg'] = "You must log in first";
-		header('location: login.php');
-	}
-
-?>
    <!DOCTYPE html>
    <html lang="en">
    <head>
@@ -52,6 +27,15 @@
    </head>
    <body>
    <header>
+   <?php 
+	include('functions.php');
+
+	if (!isAdmin()) {
+		$_SESSION['msg'] = "You must log in first";
+		header('location: login.php');
+	}
+
+?>
        <h1 id="headerh1">Clothing Store
        
 		<!-- notification message -->
