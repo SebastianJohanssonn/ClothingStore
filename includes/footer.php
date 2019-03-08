dbae341ff039c3e09da5a6c4e370060ad752a140
Anel Poturovic <Anel@Anels-MacBook-Pro.local>
2019-03-08 10:51:10 +0100

    master merge

--- includes/footer.php
+++ includes/footer.php
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-<?php
-function ClothingStore($startYear) {
-    $currentYear = date('Y');
-    if ($startYear < $currentYear) {
-        $currentYear = date('y');
-        return "&copy; $startYear&ndash;$currentYear";
-    } else {
-        return "&copy; $startYear";
-    }
-}
-
-?>
-
-<footer>
-    <ul>
-        <li>info@cs.com</li>
-        <li id=footerYear> <?php echo ClothingStore(2018); ?></li>
-        <li>01 23 456 789</li>
-    </ul>
-</footer>
-
-</html>
-
-=======
 <?php
 function ClothingStore($startYear) {
     $currentYear = date('Y');
@@ -60,4 +35,3 @@ function ClothingStore($startYear) {
 
 </html>
 
->>>>>>> master
