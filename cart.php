2458801a0b09244b4dc0ccd97247f5b296b4c079
Anel Poturovic <Anel@Anels-MacBook-Pro.local>
2019-03-08 12:05:41 +0100

    test

--- cart.php
+++ cart.php
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-<?php include ('includes/header.php') ?>
-<body>
-<div class="mainBody">
-    <h2>Shopping Cart</h2>
-    <button id="1" onclick="deleteFromShoppingcart(this)">Delete</button>
-</div>
-</body>
-=======
 <?php include ('includes/header.php') ?>
 <body onload="getAndDisplayShoppingcart()">
 <div class="mainBody">
@@ -16,5 +7,4 @@
     </div>
 </div>
 </body>
->>>>>>> master
 <?php include ('includes/footer.php') ?>
