ed03eb9a2428e248151629ebed2176b439c9369e
vogelsara <sari.vogel.94@gmail.com>
2019-03-08 11:11:44 +0100

    change the name of index-css

--- index.php
+++ index.php
@@ -24,7 +24,6 @@
 
 <div class="mainBody">
     <img id="headerImage" src="img/clothingStoreImg.jpg" alt="imageHomePage">
-
     <h2>Our Values</h2>
     <p id= "valuesText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium provident labore, repellendus voluptate alias quod, obcaecati vitae nihil distinctio ipsum accusamus! Nisi harum rem reprehenderit ipsam numquam voluptatum nostrum dicta?</p>
 </div>
