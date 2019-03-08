ce7e4cc2e1f18eaa4f8e8cf7b95603778b2257be
vogelsara <sari.vogel.94@gmail.com>
2019-03-08 11:23:51 +0100

    Remove things

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<?php include ('includes/header.php') ?>
-<body>
 <!DOCTYPE html>
 <html lang="en">
 <body>
-    
+    <?php include ('includes/header.php') ?>
+
     <!-- notification message -->
     <?php if (isset($_SESSION['success'])) : ?>
         
@@ -22,12 +21,12 @@
     <a href="index.php?logout='1'">logout</a>
     <?php endif ?> 
 
-<div class="mainBody">
-    <img id="headerImage" src="img/clothingStoreImg.jpg" alt="imageHomePage">
-    <h2>Our Values</h2>
-    <p id= "valuesText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium provident labore, repellendus voluptate alias quod, obcaecati vitae nihil distinctio ipsum accusamus! Nisi harum rem reprehenderit ipsam numquam voluptatum nostrum dicta?</p>
-</div>
+    <div class="mainBody">
+        <img id="headerImage" src="img/clothingStoreImg.jpg" alt="imageHomePage">
+        <h2>Our Values</h2>
+        <p id= "valuesText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium provident labore, repellendus voluptate alias quod, obcaecati vitae nihil distinctio ipsum accusamus! Nisi harum rem reprehenderit ipsam numquam voluptatum nostrum dicta?</p>
+    </div>
+    <?php include ('includes/footer.php') ?>
 </body>
-<?php include ('includes/footer.php') ?>
 </html>
 
