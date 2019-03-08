64a5a8b95104e9acb0870b15b0989d42fb166a58
Anel Poturovic <Anel@Anels-MacBook-Pro.local>
2019-03-08 11:48:08 +0100

    l

--- includes/header.php
+++ includes/header.php
@@ -9,15 +9,11 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-<<<<<<< HEAD
-    <link rel="stylesheet" type="text/css" href="homePage.css">
-=======
     <link rel="stylesheet" type="text/css" href="css/pc.css">
     <link rel="stylesheet" type="text/css" href="css/shoppingcart.css">
     <link rel="stylesheet" type="text/css" href="css/homePageDesktop.css">
     <link href="http://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="css/newsletter.css">
->>>>>>> 1be3cb399eb83febcf78e7b414feaf02d835bf74
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="script/cart.js"></script>
