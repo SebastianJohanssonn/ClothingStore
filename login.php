0c67c41e1000a44d384323507276f32d6dfecdbc
Anel Poturovic <you@example.com>
2019-03-11 17:51:48 +0100

    fixes and cleanup

--- login.php
+++ login.php
@@ -5,25 +5,26 @@
 
 <h1>Log in form</h1>
 
-<form method="post" action="login.php">
 
 <?php echo display_error(); ?>
+<div class="container">
 
-    <div id="input">
-        <div class="input-group">
-            <label>Username</label>
-            <input type="text" name="username" >
-            
+    <form method="post" action="login.php">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+                </div>
+            </div>
+                <button type="submit" class="text-center btn btn-primary" style="margin-left:auto;margin-right:auto;margin-bottom:20px;" name="login_btn">Login</button>
         </div>
-        <div class="input-group">
-            <label>Password</label>
-            <input type="password" name="password">
-        </div>
-    </div>
+    </form>
 
-    <button type="submit" class="btn" name="login_btn">Login</button> 
-
-</form>
 </div>
 
 </body>
