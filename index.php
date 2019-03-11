b3fcecb8e318e09f62f5033fb1a0b98e98bc1b68
Sari <sari.vogel.94@gmail.com>
2019-03-11 16:36:56 +0100

    Fix index.php

--- index.php
+++ index.php
@@ -10,9 +10,11 @@
 <?php include ('includes/header.php') ?>         
 
 <div class="mainBody">
-    <img id="headerImage" src="img/clothingStoreImg.jpg" alt="imageHomePage">
-    <h2>Our Values</h2>
-    <p id= "valuesText">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium provident labore, repellendus voluptate alias quod, obcaecati vitae nihil distinctio ipsum accusamus! Nisi harum rem reprehenderit ipsam numquam voluptatum nostrum dicta?</p>
+	<img id="headerImage" src="img/clothingStoreImg.jpg" alt="imageHomePage">
+	<div class="section">
+		<h2>Our Values</h2>
+		<p id= "valuesText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse quis lacus in purus dictum ultricies. Donec gravida efficitur congue. Mauris semper orci tristique nisl eleifend scelerisque. Aliquam venenatis faucibus tristique. Duis quis ipsum dignissim, mattis urna sit amet, aliquam risus. Maecenas accumsan vel urna eget bibendum. Etiam feugiat turpis id dui mattis, vel fringilla risus efficitur. Ut in lectus ut libero tempor convallis quis sit amet nisi. Aliquam sit amet tellus scelerisque, hendrerit lorem nec, pulvinar neque. Donec ultrices, sem vel accumsan luctus, magna sem suscipit risus, non interdum diam orci hendrerit est. Cras viverra feugiat ornare. Aenean tincidunt rhoncus lectus eget blandit. Nullam tincidunt ex quis massa venenatis iaculis. Maecenas quis tellus ut odio tincidunt eleifend. Integer pulvinar est nec enim ultricies, sed tempor justo fermentum. Nunc vitae enim egestas sapien tempus porta.</p>
+	</div>
 </div>
 <?php include ('includes/footer.php')?>
 </body>
