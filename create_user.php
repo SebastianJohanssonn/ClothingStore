2d16ae2da260b91cf2cfb5f15eafe08066466468
Anel Poturovic <Anel@Anels-MacBook-Pro.local>
2019-03-12 12:01:57 +0100

    Remvoed styles from craete user

--- create_user.php
+++ create_user.php
@@ -7,20 +7,6 @@
 		header('location: login.php');
     }
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Registration system PHP and MySQL - Create user</title>
-	<link rel="stylesheet" type="text/css" href="../style.css">
-	<style>
-		.header {
-			background: #003366;
-		}
-		button[name=register_btn] {
-			background: #003366;
-		}
-	</style>
-</head>
 <body>
 	<div class="header">
 		<h2>Admin - create user</h2>
